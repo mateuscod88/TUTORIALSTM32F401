4e6dcb0b4026b9a47c185eb48bd75970976ff394
Mateusz Malinowski <mateuscod88@gmail.com>
2017-05-02 11:28:16 +0200

    RCC_ClkInitTypeDef added

--- main.c
+++ main.c
@@ -12,4 +12,11 @@ int main()
 	rcc_osc.PLL.PLLQ = 7;
 	HAL_RCC_OscConfig(&rcc_osc);
 	
+	RCC_ClkInitTypeDef rcc_clock;
+	rcc_clock.ClockType =(RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+	rcc_clock.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	rcc_clock.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	rcc_clock.APB1CLKDivider = RCC_HCLK_DIV4;
+	rcc_clock.APB2CLKDivider = RCC_HCLK_DIV2;
+	HAL_RCC_ClockConfig(&rcc_clock,FLASH_LATENCY_5);
 }
